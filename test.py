7657970934db8a023e756822b3959a2b8eee97f6
rocio <rocio@simula.no>
2018-02-09 14:40:20 +0100

    Add test compex numbers

--- test.py
+++ test.py
@@ -25,3 +25,11 @@ def test_long():
     obs = mean4(range(1,big))
     exp = big/2.
     assert obs == exp
+
+def test_compex():
+    #testing for complex numbers
+    #as arithmetic eman of complex numbers is meaningless
+    num_list = [2+3j, 3 +4j, -32-2j]
+    obs = mean4(num_list)
+    exp = NotImplemented
+    assert obs == exp
